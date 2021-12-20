9ae30f55cc31b7591a9a77c7a1750f52f92ae0e3
kate <1195054139@qq.com>
2021-12-20 22:01:20 +0800

    first commit

---
+++ src/main/java/com/htt/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.htt.git;
+
+/**
+ * @author kate
+ * @create 2021-12-20 21:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
