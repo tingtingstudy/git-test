c1b3b62ca9c820ff092273d6dcb7461a910b3bea
kate <1195054139@qq.com>
2021-12-21 15:12:15 +0800

    master commit

--- src/main/java/com/htt/git/GitTest.java
+++ src/main/java/com/htt/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("master commit");
         System.out.println("hot-fix commit");
+        System.out.println("master-push");
     }
 }
